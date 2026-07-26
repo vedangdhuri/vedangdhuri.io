978c96808b52e60bce628ff9247d2daca8beec78
Vedang Dhuri <netvedang@gmail.com>
2026-07-27 01:49:40 +0530

    fix(ui): prevent synchronous setState inside useEffect in Magnet component

--- components/ui/Magnet.tsx
+++ components/ui/Magnet.tsx
@@ -28,8 +28,8 @@ const Magnet: React.FC<MagnetProps> = ({
 
   useEffect(() => {
     if (disabled) {
-      setPosition({ x: 0, y: 0 });
-      return;
+      const timer = setTimeout(() => setPosition({ x: 0, y: 0 }), 0);
+      return () => clearTimeout(timer);
     }
 
     const handleMouseMove = (e: MouseEvent) => {
